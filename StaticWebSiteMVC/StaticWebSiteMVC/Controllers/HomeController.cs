2dad2a5cbbd87c94ec91dd7afdf7395ee5758bdc
Maharram Aliyev <maharram.aliyev3@mail.ru>
2019-05-22 20:22:23 +0500

    Static template

---
+++ StaticWebSiteMVC/StaticWebSiteMVC/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StaticWebSiteMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Route("hakkimizda")]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [Route("mehsullar")]
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        [Route("melumat")]
+        public ActionResult Store()
+        {
+            return View();
+        }
+    }
+}
